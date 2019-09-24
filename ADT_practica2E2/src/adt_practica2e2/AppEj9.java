47d66f600ee9ea65cd7dd7985a7e1b97a136e87f
Edgar <edgaralkugm@gmail.com>
2019-09-24 12:56:47 +0200

    Acabar ejercici9

--- ADT_practica2E2/src/adt_practica2e2/AppEj9.java
+++ ADT_practica2E2/src/adt_practica2e2/AppEj9.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,33 +23,75 @@ public class AppEj9 {
         
         Scanner s = new Scanner(System.in);
         File archivo = new File("ejercicio9.txt");
-        
+        String[] valores;
+        String nombreJugador;
+        int puntuacion;
         
         try {
             
             BufferedReader br = new BufferedReader(new FileReader(archivo));
-            String nombre;
-            int puntuacion;
+            
             String linea;
             while ((linea = br.readLine())!= null) {
-                System.out.println(linea);
+                valores = linea.split(":");
+                Jugador j = new Jugador(valores[0],Integer.parseInt(valores[1]));
+                listaJugadores.add(j);
             }
             
             br.close();
-                   
 
-
+            System.out.println("Introduce tu nombre: ");
+            nombreJugador = s.next();
+            System.out.println("Introduce la puntuacion: ");
+            puntuacion = s.nextInt();
             
-            String nombreJugador;
-
-//            System.out.println("Introduce tu nombre: ");
-//            nombreJugador = s.next();
-//            System.out.println("Introduce la puntuacion: ");
-//            puntuacion = s.nextInt();
+            Jugador nuevo = new Jugador(nombreJugador,puntuacion);
+            boolean siRanking = false;
+            
+            for (int i = 0; i < listaJugadores.size(); i++) {
+                if (listaJugadores.get(i).getPuntuacion()<nuevo.getPuntuacion()) {
+                    siRanking = true;
+                }
+            }
+            
+            listaJugadores.sort(new Comparator<Jugador>() {
+                @Override
+                public int compare(Jugador o1, Jugador o2) {
+                    if (o1.getPuntuacion()>o2.getPuntuacion()) {
+                        return -1;
+                    }else
+                        return 0;
+                }
+            });
+            
+            if (siRanking) {
+                listaJugadores.remove(listaJugadores.get(listaJugadores.size()-1));
+                listaJugadores.add(nuevo);
+            }
+            
+            listaJugadores.sort(new Comparator<Jugador>() {
+                @Override
+                public int compare(Jugador o1, Jugador o2) {
+                    if (o1.getPuntuacion()>o2.getPuntuacion()) {
+                        return -1;
+                    }else
+                        return 0;
+                }
+            });
+            
+            
             
             PrintWriter pw = new PrintWriter(archivo);
             
-//            pw.write(nombreJugador+" "+puntuacion);
+            for (int i = 0; i < listaJugadores.size(); i++) {
+                pw.println(listaJugadores.get(i).getNombre()+":"+listaJugadores.get(i).getPuntuacion());
+            }
+
+
+            for (int i = 0; i < listaJugadores.size(); i++) {
+                System.out.println(listaJugadores.get(i).getNombre() + " > " + listaJugadores.get(i).getPuntuacion());
+            }
+
             br.close();
             pw.close();
         } catch (FileNotFoundException ex) {
