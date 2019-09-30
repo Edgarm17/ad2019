bd9c97654cfc936a5e7fccac0f07c9bf82f13413
Edgar <edgaralkugm@gmail.com>
2019-09-30 18:28:54 +0200

    Ejercici2 practica 3

---
+++ ADT3_Practica1/src/adt3_practica1/Ejercicio1.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adt3_practica1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author vesprada
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        File f = new File("ejercicio1.xml");
+        
+        DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
+        try {
+            DocumentBuilder builder = builderFactory.newDocumentBuilder();
+            Document document = builder.parse(f);
+            
+            Element rootElement = document.getDocumentElement();
+            
+            NodeList lista = document.getElementsByTagName("prestamo");
+           
+            Element prestamo = (Element) lista.item(0);
+            
+            Element libro = document.createElement("libro");
+            
+            Element titulo = document.createElement("titulo");
+            Text textoTitulo = document.createTextNode("Acces a Dades");
+            titulo.appendChild(textoTitulo);
+            
+            Element autor = document.createElement("autor");
+            Text textoAutor = document.createTextNode("Edgar");
+            autor.appendChild(textoAutor);
+            
+            Element anyo = document.createElement("anyo");
+            Text textoAnyo = document.createTextNode("2019");
+            anyo.appendChild(textoAnyo);
+            
+            Element editorial = document.createElement("editorial");
+            Text textoEditorial = document.createTextNode("Simarro");
+            editorial.appendChild(textoEditorial);
+            
+            libro.setAttribute("id", "asdf1234");
+            
+            
+            libro.appendChild(titulo);
+            libro.appendChild(autor);
+            libro.appendChild(anyo);
+            libro.appendChild(editorial);
+            
+            prestamo.appendChild(libro);
+            
+            
+            Transformer trans = TransformerFactory.newInstance().newTransformer();
+            trans.setOutputProperty(OutputKeys.METHOD, "xml");
+            trans.setOutputProperty(OutputKeys.INDENT, "yes");
+
+            DOMSource source = new DOMSource(document);
+            StreamResult result = new StreamResult(new File("ejercicio1.xml"));
+            trans.transform(source, result);
+
+            System.out.println("Fichero modificado.");
+            
+            
+            
+            
+            
+            
+            
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerConfigurationException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(Ejercicio1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ ADT3_Practica1/src/adt3_practica1/Ejercicio2.java
@@ -0,0 +1,93 @@
+package adt3_practica1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author vesprada
+ */
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        try {
+            DocumentBuilder docBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document docNuevo = docBuilder.newDocument();
+            
+            Element alumnos = docNuevo.createElement("alumnos");
+
+            Element alumno = docNuevo.createElement("alumno");
+
+            Element nombre = docNuevo.createElement("nombre");
+            
+            Element apellido = docNuevo.createElement("apellido");
+            
+            Element edad1 = docNuevo.createElement("edad");
+            
+            
+            nombre.setTextContent("Juan");
+            apellido.setTextContent("Martí López");
+            edad1.setTextContent("18");
+            
+            alumno.appendChild(nombre);
+            alumno.appendChild(apellido);
+            alumno.appendChild(edad1);
+            alumnos.appendChild(alumno);
+            
+            alumno = docNuevo.createElement("alumno");
+            
+            nombre = docNuevo.createElement("nombre");
+            
+            apellido = docNuevo.createElement("apellido");
+            
+            edad1 = docNuevo.createElement("edad");
+            
+            nombre.setTextContent("Juan");
+            apellido.setTextContent("Martí López");
+            edad1.setTextContent("18");
+            alumno.appendChild(nombre);
+            alumno.appendChild(apellido);
+            alumno.appendChild(edad1);
+            alumnos.appendChild(alumno);
+            
+            
+
+            docNuevo.appendChild(alumnos);
+
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+
+            Transformer transformer = transformerFactory.newTransformer();
+
+            DOMSource source = new DOMSource(docNuevo);
+
+            StreamResult result = new StreamResult(new File("ejercicio2.xml"));
+            transformer.transform(source, result);
+            
+            System.out.println("Arxiu creat!");
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (TransformerException ex) {
+            Logger.getLogger(Ejercicio2.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
