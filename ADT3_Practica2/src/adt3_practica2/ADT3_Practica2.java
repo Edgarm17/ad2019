a25ea7bd29fc03f74e61a6048a0db1227c885895
Edgar <edgaralkugm@gmail.com>
2019-10-11 12:40:38 +0200

    Commit practica 3 SAX

--- ADT3_Practica2/src/adt3_practica2/ADT3_Practica2.java
+++ ADT3_Practica2/src/adt3_practica2/ADT3_Practica2.java
@@ -6,10 +6,19 @@
 package adt3_practica2;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
 
 /**
  *
@@ -24,8 +33,40 @@ public class ADT3_Practica2 {
         
         File archivo = new File("ejercicio1.xml");
         
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser parser = factory.newSAXParser();
+//        SAXParserFactory factory = SAXParserFactory.newInstance();
+//        SAXParser parser = factory.newSAXParser();
+        ArrayList<Receta> principales = new ArrayList<>();
+        ArrayList<Receta> secundarios = new ArrayList<>();
+        ArrayList<Receta> postres = new ArrayList<>();
+        
+        try{
+            
+            
+            XMLReader reader = XMLReaderFactory.createXMLReader();
+            reader.setContentHandler(new ManejadorRecetas(principales,secundarios,postres));
+            reader.parse(new InputSource(new FileInputStream(archivo)));
+            
+            System.out.println("Principales: ");
+            for (int i = 0; i < principales.size(); i++) {
+                principales.get(i).toString();
+            }
+            System.out.println("");
+            System.out.println("Secundarios: ");
+            for (int i = 0; i < secundarios.size(); i++) {
+                secundarios.get(i).toString();
+            }
+            System.out.println("");
+            System.out.println("Postres: ");
+            for (int i = 0; i < postres.size(); i++) {
+                postres.get(i).toString();
+            }
+        } catch (Exception e) {
+            
+            String errorMessage = "Error analizando "+archivo+": "+ e;
+            System.err.println(errorMessage);
+            e.printStackTrace();
+            
+        } 
     }
     
 }
---
+++ ADT3_Practica2/src/adt3_practica2/ManejadorRecetas.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adt3_practica2;
+
+import java.util.ArrayList;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ *
+ * @author vesprada
+ */
+public class ManejadorRecetas extends DefaultHandler {
+
+    private ArrayList<Receta> principales;
+    private ArrayList<Receta> secundarios;
+    private ArrayList<Receta> postre;
+    private Receta receta;
+    private String valor = null;
+
+    public ManejadorRecetas(ArrayList<Receta> principales,ArrayList<Receta> secundarios,ArrayList<Receta> postre) {
+        this.principales = principales;
+        this.secundarios = secundarios;
+        this.postre = postre;
+    }
+
+    @Override
+    public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+        valor = null;
+
+        if (localName.equals("receta")) {
+            receta = new Receta();
+            String tipo = attributes.getValue("tipo");
+            System.out.println(tipo);
+            receta.setTipo(tipo);
+            
+            switch (receta.getTipo()) {
+                case "principal":
+                    principales.add(receta);
+                    break;
+                case "secundario":
+                    secundarios.add(receta);
+                    break;
+                case "postre":
+                    postre.add(receta);
+                    break;
+            }
+            
+            
+            
+        }
+    }
+
+    @Override
+    public void characters(char[] ch, int start, int length) throws SAXException {
+        valor = new String(ch, start, length);
+    }
+
+    @Override
+    public void endElement(String uri, String localName, String qName) throws SAXException {
+        if (localName.equals("nombre")) {
+            receta.setNombre(valor);
+        } else if (localName.equals("descripcion")) {
+            receta.setDescripcion(valor);
+        }
+        
+        
+    }
+
+}
